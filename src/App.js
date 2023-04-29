19cc862a8b8b6d2c75f026bba865bc73d8721701
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-04-29 07:52:31 -0700

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">hello world</header>
+      <Header />
     </div>
   );
 }
